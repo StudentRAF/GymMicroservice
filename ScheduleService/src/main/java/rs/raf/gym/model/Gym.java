750b7a089bb0a81942d311bb8cf8d50d8e2ae428
StudentRAF <nradovanovic12022rn@raf.rs>
2023-12-18 02:41:47 +0100

    Implement gym database model

---
+++ ScheduleService/src/main/java/rs/raf/gym/model/Gym.java
@@ -0,0 +1,58 @@
+/*
+ * Copyright (C) 2023. Lazar Dobrota and Nemanja Radovanovic
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package rs.raf.gym.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.PositiveOrZero;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.Objects;
+
+@Setter
+@Getter
+@Entity
+@Table(name = "gym")
+public class Gym {
+
+    @Id
+    @Column(length = 30)
+    private String name;
+
+    @Column(length = 1000)
+    private String description;
+
+    @NotNull
+    private Long managerId;
+
+    @NotNull
+    @PositiveOrZero
+    private Integer trainers;
+
+    @Override
+    public boolean equals(Object object) {
+        if (object instanceof Gym gym)
+            return Objects.equals(gym.getName(), name);
+
+        return false;
+    }
+
+}
