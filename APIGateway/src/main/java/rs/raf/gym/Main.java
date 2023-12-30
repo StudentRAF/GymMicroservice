83eaca516ac12e4386c40635d52d1d049c4cc9cb
StudentRAF <nradovanovic12022rn@raf.rs>
2023-12-30 22:09:13 +0100

    API Gateway - Implement routing through eureka discovery service

--- APIGateway/src/main/java/rs/raf/gym/Main.java
+++ APIGateway/src/main/java/rs/raf/gym/Main.java
@@ -18,6 +18,9 @@ package rs.raf.gym;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.context.annotation.PropertySources;
 
@@ -30,6 +33,21 @@ public class Main {
 
     public static void main(String[] args) {
         SpringApplication.run(Main.class, args);
+    }
+
+    @Bean
+    public RouteLocator customRouteLocator(RouteLocatorBuilder builder) {
+        return builder.routes()
+                      .route(predicate -> predicate.path("/api/schedule/**")
+                                                   .filters(filter -> filter.rewritePath("/api/schedule/(?<route>.*)", "/schedule/${route}"))
+                                                   .uri("lb://SCHEDULE-SERVICE"))
+                      .route(predicate -> predicate.path("/api/user/**")
+                                                   .filters(filter -> filter.rewritePath("/api/user/(?<route>.*)", "/user/${route}"))
+                                                   .uri("lb://USER-SERVICE"))
+                      .route(predicate -> predicate.path("/api/notification/**")
+                                                   .filters(filter -> filter.rewritePath("/api/notification/(?<route>.*)", "/notification/${route}"))
+                                                   .uri("lb://NOTIFICATION-SERVICE"))
+                      .build();
     }
 
 }
